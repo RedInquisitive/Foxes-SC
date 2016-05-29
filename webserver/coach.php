247fcf481a61e772fd8346c28e111f4f3af4e431
AJ Walter <aj.walter@aol.com>
2016-05-29 13:03:30 -0500

    Fix head and remove php.

--- webserver/coach.php
+++ webserver/coach.php
@@ -41,12 +41,12 @@ if(isset($_SESSION['login_user']) && $_SESSION['valid'] === "Coach"){
 ?>
 <!DOCTYPE html>
 <html>
-<header>
+<head>
 	<title>Coach Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-</header>
+</head>
 <body>
 	<div id="main">
 		<div id="login">
--- webserver/student.php
+++ webserver/student.php
@@ -1,57 +1,55 @@
-<!-- 
 <?php
-include '/home/aj4057/config.php'; #Define $servername $username $password $dbname and $configready here.
-include '/home/aj4057/indexkeys.php'; #Index keys that are used. For example, Index::REQUEST is defined here.
+// include '/home/aj4057/config.php'; #Define $servername $username $password $dbname and $configready here.
+// include '/home/aj4057/indexkeys.php'; #Index keys that are used. For example, Index::REQUEST is defined here.
 
-do {
-session_start(); #Starting Session
-$error=''; #Variable To Store Error Message
+// do {
+// session_start(); #Starting Session
+// $error=''; #Variable To Store Error Message
 
-try {
-	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password); #login
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); #Enable errors
-} catch(PDOException $e) {
-	$error = "Could not connect to the database. This should never happen.";
-	break;
-}
+// try {
+	// $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password); #login
+	// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); #Enable errors
+// } catch(PDOException $e) {
+	// $error = "Could not connect to the database. This should never happen.";
+	// break;
+// }
 
-if (!empty($_POST)) {
-	if(!(array_key_exists(Student::ID,$_POST))) {
-		$error = "You need to enter a Student ID";
-		break;
-	}
-	if($_POST[Student::ID] === '') {
-		$error = "You need to enter a Student ID";
-		break;
-	}
-	$stmt = $conn->prepare("SELECT STUDENT_ID FROM STUDENT WHERE STUDENT_ID = :id"); #select data
-	$stmt->execute(array('id' => $_POST[Student::ID])); #based on the ID
-	$row = $stmt->fetch();
-	if($row["STUDENT_ID"] !== $_POST[Student::ID]) {
-		$error = "The username or password is incorrect!"; #Deny it.
-		break;
-	}
-	$_SESSION['login_user'] = $row["STUDENT_ID"]; #Initializing Session
-	$_SESSION['timestamp'] = date("Y-m-d H:i:s"); #Initializing Session
-	$_SESSION['valid'] = "Student";
-	header("location: student/index.php"); #Redirecting To Other Page
-	$conn = null;
-}
+// if (!empty($_POST)) {
+	// if(!(array_key_exists(Student::ID,$_POST))) {
+		// $error = "You need to enter a Student ID";
+		// break;
+	// }
+	// if($_POST[Student::ID] === '') {
+		// $error = "You need to enter a Student ID";
+		// break;
+	// }
+	// $stmt = $conn->prepare("SELECT STUDENT_ID FROM STUDENT WHERE STUDENT_ID = :id"); #select data
+	// $stmt->execute(array('id' => $_POST[Student::ID])); #based on the ID
+	// $row = $stmt->fetch();
+	// if($row["STUDENT_ID"] !== $_POST[Student::ID]) {
+		// $error = "The username or password is incorrect!"; #Deny it.
+		// break;
+	// }
+	// $_SESSION['login_user'] = $row["STUDENT_ID"]; #Initializing Session
+	// $_SESSION['timestamp'] = date("Y-m-d H:i:s"); #Initializing Session
+	// $_SESSION['valid'] = "Student";
+	// header("location: student/index.php"); #Redirecting To Other Page
+	// $conn = null;
+// }
 
-if(isset($_SESSION['login_user']) && $_SESSION['valid'] = "Student"){
-	header("location: student/index.php");
-}
-} while (0); #but it works!
+// if(isset($_SESSION['login_user']) && $_SESSION['valid'] = "Student"){
+	// header("location: student/index.php");
+// }
+// } while (0); #but it works!
 ?>
--->
 <!DOCTYPE html>
 <html>
-<header>
+<head>
 	<title>Student Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-</header>
+</head>
 <body>
 	<div id="main">
 		<div id="login">
